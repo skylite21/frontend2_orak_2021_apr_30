d51fcabdc425592a82a414e6aa3bbeb68f144c82
Zsolt Lengyel <zsolt.lengyel21@gmail.com>
2021-05-29 11:02:59 +0200

    05_29_2021_11_02_59 fast commit

--- chat-starter/src/app.js
+++ chat-starter/src/app.js
@@ -3,6 +3,7 @@ import './scss/style.scss';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import config from './db_config.js';
+import scrolIntoView from 'scroll-into-view-if-needed';
 
 
 const firedb = firebase.initializeApp(config);
@@ -10,6 +11,7 @@ const db = firedb.firestore();
 
 async function sendMessage(data) {
   const res = await db.collection('messages').add(data);
+  document.querySelector('#message').value = '';
   console.log(res);
 }
 
@@ -33,26 +35,66 @@ function displayMessage(message) {
       </div>
   `; 
   document.querySelector('#messages').insertAdjacentHTML('beforeend', messageDOM);
+  scrolIntoView(document.querySelector('#messages'), {
+    scrollMode: 'if-needed',
+    block: 'end'
+  });
 
 }
 
 function createMessage() {
   const message = document.querySelector('#message').value;
   const username = document.querySelector('#nickname').value;
+  const date = firebase.firestore.Timestamp.fromDate(new Date());
   // ha a változó neve ugyanaz mint a key amit létre akarunk hozni
   // az objectben akkor nem kell kétszer kiírni...
-  return { message, username };
+  return { message, username, date };
+}
+
+
+async function displayAllMessages() {
+  const query = await db.collection('messages').orderBy('date', 'asc').get();
+  query.forEach((doc) => {
+    displayMessage(doc.data());
+  });
+}
+
+function handleMessage() {
+  const message = createMessage();
+  if(message.username && message.message) {
+    sendMessage(message);
+    // displayMessage(message);
+  }
 }
 
 // amikor a html teljesen betölt: 
 window.addEventListener('DOMContentLoaded', () => {
-  
+  // displayAllMessages(); 
   document.querySelector('#send').addEventListener('click', () => {
-    const message = createMessage();
-    if(message.username && message.message) {
-      sendMessage(message);
-      displayMessage(message);
-    }
+    handleMessage();
   });
 });
+
+document.addEventListener('keyup', (event) => {
+  if(event.key === 'Enter') {
+    handleMessage();
+  }
+});
+
+
+// listen for changes in the database
+db.collection('messages').orderBy('date', 'asc')
+  .onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === 'added') {
+        displayMessage(change.doc.data());
+      }
+      if (change.type === 'modified') {
+        console.log('Modified message: ', change.doc.data());
+      }
+      if (change.type === 'removed') {
+        console.log('Removed message: ', change.doc.data());
+      }
+    });
+  });
 
